cb09d52cd12b583b18c51a023f7dbd34f36510ce
FedoseevaAlexandra <70970063+FedoseevaAlexandra@users.noreply.github.com>
2020-11-24 21:40:12 +0200

    Update string.3.py

--- string.3.py
+++ string.3.py
@@ -2,7 +2,7 @@ a=str(input('cum viata? '))
 b=str(input('ce vrei acum? '))
 c=str(input("ce mancare vrei? "))
 d=str(input('ce mancare vrei?.2 '))
-if (len(list(a))>1)or(len(list(b))>1)or(len(list(c))>1)or(len(list(d))>1):
+if (len(a.split(' '))>1) or(len(b.split(' '))>1)or(len(c.split(' '))>1)or(len(d.split(' '))>1):
     print('raspunde printr-un cuvant')
 else:
     print('cuvintele erau :',a,b,c,d)
